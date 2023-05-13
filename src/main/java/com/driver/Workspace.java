d4562c99bd68723cd0d6a9edde4ede8d6ee970cd
unknown <faizkh97@gmail.com>
2023-05-13 03:01:49 -0700

    clear the error

--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -4,8 +4,7 @@ import org.apache.commons.lang3.tuple.Pair;
 
 import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+
 
 public class Workspace extends Gmail{
 
@@ -29,39 +28,26 @@ public class Workspace extends Gmail{
         // 1. At a particular time, you can be present in at most one meeting
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
-        Collections.sort(calendar, Comparator.comparing(Meeting::getStartTime));
-
-        int maxMeetings = 0;
-        int currentMeetings = 0;
-        LocalTime currentTime = LocalTime.MIN;
-
-        for (Meeting meeting : calendar) {
-            // If the meeting starts after the current time, we can attend it
-            if (meeting.getStartTime().isAfter(currentTime)) {
-                currentMeetings++;
-                currentTime = meeting.getEndTime();
-
-                // Update the maximum number of meetings
-                maxMeetings = Math.max(maxMeetings, currentMeetings);
-            } else {
-                // If the meeting has already started, we cannot attend it
-                // We need to find a meeting that starts after the current meeting ends
-                int i = calendar.indexOf(meeting);
-                while (i < calendar.size() && meeting.getEndTime().isAfter(calendar.get(i).getStartTime())) {
-                    i++;
-                }
-
-                if (i < calendar.size()) {
-                    // We found a meeting that starts after the current meeting ends
-                    currentMeetings--;
-                    currentTime = calendar.get(i).getEndTime();
-                } else {
-                    // We reached the end of the calendar, so there are no more meetings to attend
-                    break;
-                }
+        Meeting[] sortedMeetings = calendar.toArray(new Meeting[calendar.size()]);
+        for (int i = 1; i < sortedMeetings.length; i++) {
+            Meeting currentMeeting = sortedMeetings[i];
+            int j = i - 1;
+            while (j >= 0 && sortedMeetings[j].getEndTime().compareTo(currentMeeting.getEndTime()) > 0) {
+                sortedMeetings[j + 1] = sortedMeetings[j];
+                j--;
             }
+            sortedMeetings[j + 1] = currentMeeting;
         }
 
+        // Greedily select the maximum number of non-overlapping meetings
+        int maxMeetings = 0;
+        LocalTime lastEndTime = null;
+        for (Meeting meeting : sortedMeetings) {
+            if (lastEndTime == null || meeting.getStartTime().compareTo(lastEndTime) >= 0) {
+                maxMeetings++;
+                lastEndTime = meeting.getEndTime();
+            }
+        }
         return maxMeetings;
     }
 }
